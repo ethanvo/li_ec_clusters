211261a34f4c5a761014bb06a9364ac2fbbd87ef
Ethan Vo <ethan.vo@hotmail.com>
2025-03-18 12:17:46 -0400

    made clusters

--- HY_cluster/reaction/carving.py
+++ HY_cluster/reaction/carving.py
@@ -13,17 +13,17 @@ if __name__ == '__main__':
         Rxyz = sys.argv[1]
         TSxyz = sys.argv[2]
         mol_atmidx = np.asarray(list(map(int, sys.argv[3].split(',')))) if sys.argv[3].lower() != "none" else np.asarray([])
-        rcenter_atmidx = np.asarray(list(map(int, sys.argv[4].split(','))))
+        center_atmidx = np.asarray(list(map(int, sys.argv[4].split(','))))
         Rout_path = sys.argv[5]
         TSout_path = sys.argv[6]
     except:
         log.warn('Usage: Rxyz, TS_xyz, mol_atmidx, center_atmidx, Rout_prefix, TSout_prefix')
         sys.exit(1)
 
-    ratom = '\n'.join(open(rxyz, 'r').read().lstrip('\n').rstrip('\n').split('\n')[2:])
+    ratom = '\n'.join(open(Rxyz, 'r').read().lstrip('\n').rstrip('\n').split('\n')[2:])
     rmol = gto.M(atom=ratom, basis='cc-pvdz', spin=None)
 
-    tsatom = '\n'.join(open(tsxyz, 'r').read().lstrip('\n').rstrip('\n').split('\n')[2:])
+    tsatom = '\n'.join(open(TSxyz, 'r').read().lstrip('\n').rstrip('\n').split('\n')[2:])
     tsmol = gto.M(atom=tsatom, basis='cc-pvdz', spin=None)
 
     rsurf_atmidx = np.asarray([i for i in range(rmol.natm) if i not in mol_atmidx])
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     rsurf_rs = rrs[rsurf_atmidx].reshape(-1,3)
     tssurf_rs = tsrs[tssurf_atmidx].reshape(-1,3)
     
-    center_rs = np.concatenate((rrs[rcenter_atmidx],tsrs[tscenter_atmidx])).reshape(-1,3).mean(axis=0)
+    center_rs = np.concatenate((rrs[center_atmidx],tsrs[center_atmidx])).reshape(-1,3).mean(axis=0)
     print(center_rs)
     dist = np.linalg.norm(rsurf_rs - center_rs, axis=-1)
     order = np.argsort(dist)
@@ -53,7 +53,7 @@ if __name__ == '__main__':
         # keep_atmidx = keep_atmidx[np.argsort(rs[keep_atmidx,2])]
         # keep_atmidx = np.concatenate((keep_atmidx, mol_atmidx)).astype(int)
 
-        keep_atom = [(atms[i],rs[i]) for i in keep_atmidx]
+        keep_atom = [(ratms[i],rrs[i]) for i in keep_atmidx]
 
         # fout = f'{out_prefix}_{rcut:.1f}.xyz'
         fout = f'{Rout_path}/{nsurf}.xyz'
@@ -72,7 +72,7 @@ if __name__ == '__main__':
         # keep_atmidx = keep_atmidx[np.argsort(rs[keep_atmidx,2])]
         # keep_atmidx = np.concatenate((keep_atmidx, mol_atmidx)).astype(int)
 
-        keep_atom = [(atms[i],rs[i]) for i in keep_atmidx]
+        keep_atom = [(tsatms[i],tsrs[i]) for i in keep_atmidx]
 
         # fout = f'{out_prefix}_{rcut:.1f}.xyz'
         fout = f'{TSout_path}/{nsurf}.xyz'
